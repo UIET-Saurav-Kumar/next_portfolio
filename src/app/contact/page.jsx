260d3cfd6072e6b66ec9f29576370acdce8864a0
unknown <101saurav.kumar@gmail.com>
2024-03-20 17:06:01 +0530

    contact form

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -42,7 +42,7 @@ const ContactPage = () => {
     >
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* TEXT CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
+        <div className="h-1/3 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
           <div>
             {text.split("").map((letter, index) => (
               <motion.span
